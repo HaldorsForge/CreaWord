497fd41375fe327a4d550e79885351dbc3d2e11c
Paul Jeschke <Paul-Jeschke@outlook.com>
2019-05-22 23:08:31 +0200

    extreact the datamodels in seperate files and classes

---
+++ CreaWordv4/Models/Data/Kategorie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreaWordv4.Models.Data
+{
+    public class Kategorie
+    {
+        public int ID { get; set; }
+        public String BEZ { get; set; }
+    }
+}
---
+++ CreaWordv4/Models/Data/Liste.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreaWordv4.Models.Data
+{
+    public class Liste
+    {
+        public int ID { get; set; }
+        public String Titel { get; set; }
+        public String Auswahl { get; set; }
+    }
+}
